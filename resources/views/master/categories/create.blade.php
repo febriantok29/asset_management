5e30ff17aec3e78889c9beb27b7bf39ab27b675e
febriantok29 <febriantok29@gmail.com>
2024-11-01 17:46:15 +0700

    Change category labeling to Indonesia

--- resources/views/master/categories/create.blade.php
+++ resources/views/master/categories/create.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
     <div class="container">
-        <h1 class="my-4">Add New Category</h1>
+        <h1 class="my-4">Tambah Kategori Baru</h1>
         <form action="{{ route('categories.store') }}" method="POST">
             @csrf
             <div class="form-group mb-3">
-                <label for="name">Name</label>
+                <label for="name">Nama</label>
                 <input type="text" class="form-control" name="name" required>
             </div>
             <div class="form-group mb-3">
-                <label for="description">Description</label>
+                <label for="description">Keterangan</label>
                 <textarea class="form-control" name="description" rows="4"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Save</button>
--- resources/views/master/categories/edit.blade.php
+++ resources/views/master/categories/edit.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
     <div class="container">
-        <h1 class="my-4">Edit Category</h1>
+        <h1 class="my-4">Ubah Kategori</h1>
         <form action="{{ route('categories.update', $category->id) }}" method="POST">
             @csrf
             @method('PUT')
             <div class="form-group mb-3">
-                <label for="name">Name</label>
+                <label for="name">Nama</label>
                 <input type="text" class="form-control" name="name" value="{{ $category->name }}" required>
             </div>
             <div class="form-group mb-3">
-                <label for="description">Description</label>
+                <label for="description">Keterangan</label>
                 <textarea class="form-control" name="description" rows="4">{{ $category->description }}</textarea>
             </div>
             <button type="submit" class="btn btn-primary">Update</button>
