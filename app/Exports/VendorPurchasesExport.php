61f5deb639f9fb8bc1b1bd1ec25e24a689588ad7
febriantok29-fork <febriantok29@gmail.com>
2025-01-05 11:45:44 +0700

    Improve Vendor Excel export

--- app/Exports/VendorPurchasesExport.php
+++ app/Exports/VendorPurchasesExport.php
@@ -2,15 +2,63 @@
 
 namespace App\Exports;
 
+use App\Models\Master\Vendor;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class VendorPurchasesExport implements FromCollection
+class VendorPurchasesExport implements FromCollection, WithHeadings, WithMapping, WithColumnWidths, WithStyles
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
+    protected $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
     public function collection()
     {
-        //
+        return $this->data;
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID Vendor',
+            'Nama Vendor',
+            'Total Pembelian',
+            'Pembelian Terakhir',
+        ];
+    }
+
+    public function map($vendor): array
+    {
+        return [
+            $vendor->id,
+            $vendor->name,
+            $vendor->purchases_count,
+            $vendor->last_purchase_date,
+        ];
+    }
+
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 15,
+            'B' => 30,
+            'C' => 20,
+            'D' => 20,
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            // Style the first row as bold text.
+            1 => ['font' => ['bold' => true]],
+        ];
     }
 }
